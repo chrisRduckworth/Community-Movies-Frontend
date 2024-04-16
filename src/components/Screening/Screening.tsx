4cba73c5ae35217a44afde06bd17fbde750ed287
chrisRduckworth <c_duckworth@hotmail.com>
2024-04-16 17:31:27 +0100

    remove cost from screening

--- src/components/Screening/Screening.tsx
+++ src/components/Screening/Screening.tsx
@@ -4,16 +4,14 @@ import { getScreening } from "../../utils/api";
 import { ScreeningDetail } from "../../interfaces";
 import "./Screening.css";
 import dayjs from "dayjs";
-import Cost from "./Cost";
 
-function Screening({ cost, setCost }: { cost: number; setCost: any }) {
+function Screening() {
   const { screening_id } = useParams();
   const [screening, setScreening] = useState<null | ScreeningDetail>(null);
 
   useEffect(() => {
     getScreening(screening_id).then((screeningData) => {
       setScreening(screeningData);
-      setCost(screeningData.cost);
     });
   }, []);
 
@@ -39,27 +37,29 @@ function Screening({ cost, setCost }: { cost: number; setCost: any }) {
             <div id="screening-details-container">
               <div id="screening-details">
                 <div id="screening-details-cd">
-                  <Cost
-                    cost={cost}
-                    setCost={setCost}
-                    is_pay_what_you_want={screening.is_pay_what_you_want}
-                  />
+                  <h4>Cost</h4>
+                  <p>
+                    {screening.is_pay_what_you_want
+                      ? "Pay what you want"
+                      : (screening.cost / 100).toLocaleString("en-UK", {
+                          style: "currency",
+                          currency: "GBP",
+                        })}
+                  </p>
                   <h4>Date</h4>
                   <p>{dayjs(screening.date).format("D MMMM YYYY, HH:mm")}</p>
                 </div>
                 <div id="location">
                   <h4>Location</h4>
-                  {screening.location.split(",").map((line, i) => (
+                  {screening.location.split(",").map((line, i, arr) => (
                     <p key={line}>
                       {line}
-                      {i === screening.location.split(",").length - 1
-                        ? ""
-                        : ","}
+                      {i === arr.length - 1 ? "" : ","}
                     </p>
                   ))}
                 </div>
               </div>
-              <Link to={`/screening/${screening_id}/book`} id="book-link">
+              <Link to={`/screenings/${screening_id}/book`} id="book-link">
                 Book
               </Link>
             </div>
